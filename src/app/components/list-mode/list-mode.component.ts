ceb0343ca380ac4ea0023d46d5d0d0467853ec26
elwood-berry <eberry@anterasoftware.com>
2021-07-21 07:48:59 -0500

    Updated list and static mode

--- src/app/components/list-mode/list-mode.component.ts
+++ src/app/components/list-mode/list-mode.component.ts
@@ -43,72 +43,72 @@ export interface Lead {
 const ELEMENT_DATA: Lead[] = [
   {
     leadPosition: 1, // ID
-    leadName: 'Michael Richards', // Name
-    leadEmail: 'email@email.com', // Email
-    leadSource: 'Lead Source Goes Here', // Lead leadSource
-    leadSalesRep: `Sales Rep Name Goes Here` // Sales Rep
+    leadName: 'John Doe', // Name
+    leadEmail: 'john.doe@email.com', // Email
+    leadSource: 'Miami Conference', // Lead leadSource
+    leadSalesRep: `George Costanza` // Sales Rep
   },
   {
     leadPosition: 2,
-    leadName: 'Michael Richards', // Name
-    leadEmail: 'email@email.com', // Email
-    leadSource: 'Lead Source Goes Here', // Lead leadSource
-    leadSalesRep: `Sales Rep Name Goes Here` // Sales Rep
+    leadName: 'John Doe', // Name
+    leadEmail: 'john.doe@email.com', // Email
+    leadSource: 'Miami Conference', // Lead leadSource
+    leadSalesRep: `George Costanza` // Sales Rep
   },
   {
     leadPosition: 3,
-    leadName: 'Michael Richards', // Name
-    leadEmail: 'email@email.com', // Email
-    leadSource: 'Lead Source Goes Here', // Lead leadSource
-    leadSalesRep: `Sales Rep Name Goes Here` // Sales Rep
+    leadName: 'John Doe', // Name
+    leadEmail: 'john.doe@email.com', // Email
+    leadSource: 'Miami Conference', // Lead leadSource
+    leadSalesRep: `George Costanza` // Sales Rep
   },
   {
     leadPosition: 4,
-    leadName: 'Michael Richards', // Name
-    leadEmail: 'email@email.com', // Email
-    leadSource: 'Lead Source Goes Here', // Lead leadSource
-    leadSalesRep: `Sales Rep Name Goes Here` // Sales Rep
+    leadName: 'John Doe', // Name
+    leadEmail: 'john.doe@email.com', // Email
+    leadSource: 'Miami Conference', // Lead leadSource
+    leadSalesRep: `George Costanza` // Sales Rep
   },
   {
     leadPosition: 5,
-    leadName: 'Michael Richards', // Name
-    leadEmail: 'email@email.com', // Email
-    leadSource: 'Lead Source Goes Here', // Lead leadSource
-    leadSalesRep: `Sales Rep Name Goes Here` // Sales Rep
+    leadName: 'John Doe', // Name
+    leadEmail: 'john.doe@email.com', // Email
+    leadSource: 'Miami Conference', // Lead leadSource
+    leadSalesRep: `George Costanza` // Sales Rep
   },
   {
     leadPosition: 6,
-    leadName: 'Michael Richards', // Name
-    leadEmail: 'email@email.com', // Email
-    leadSource: 'Lead Source Goes Here', // Lead leadSource
-    leadSalesRep: `Sales Rep Name Goes Here` // Sales Rep
+    leadName: 'John Doe', // Name
+    leadEmail: 'john.doe@email.com', // Email
+    leadSource: 'Miami Conference', // Lead leadSource
+    leadSalesRep: `George Costanza` // Sales Rep
   },
   {
     leadPosition: 7,
-    leadName: 'Michael Richards', // Name
-    leadEmail: 'email@email.com', // Email
-    leadSource: 'Lead Source Goes Here', // Lead leadSource
-    leadSalesRep: `Sales Rep Name Goes Here` // Sales Rep
+    leadName: 'John Doe', // Name
+    leadEmail: 'john.doe@email.com', // Email
+    leadSource: 'Miami Conference', // Lead leadSource
+    leadSalesRep: `George Costanza` // Sales Rep
   },
   {
     leadPosition: 8,
-    leadName: 'Michael Richards', // Name
-    leadEmail: 'email@email.com', // Email
-    leadSource: 'Lead Source Goes Here', // Lead leadSource
-    leadSalesRep: `Sales Rep Name Goes Here` // Sales Rep
+    leadName: 'John Doe', // Name
+    leadEmail: 'john.doe@email.com', // Email
+    leadSource: 'Miami Conference', // Lead leadSource
+    leadSalesRep: `George Costanza` // Sales Rep
   },
   {
     leadPosition: 9,
-    leadName: 'Michael Richards', // Name
-    leadEmail: 'email@email.com', // Email
-    leadSource: 'Lead Source Goes Here', // Lead leadSource
-    leadSalesRep: `Sales Rep Name Goes Here` // Sales Rep
+    leadName: 'John Doe', // Name
+    leadEmail: 'john.doe@email.com', // Email
+    leadSource: 'Miami Conference', // Lead leadSource
+    leadSalesRep: `George Costanza` // Sales Rep
   },
   {
     leadPosition: 10,
-    leadName: 'Michael Richards', // Name
-    leadEmail: 'email@email.com', // Email
-    leadSource: 'Lead Source Goes Here', // Lead leadSource
-    leadSalesRep: `Sales Rep Name Goes Here` // Sales Rep
+    leadName: 'John Doe', // Name
+    leadEmail: 'john.doe@email.com', // Email
+    leadSource: 'Miami Conference', // Lead leadSource
+    leadSalesRep: `George Costanza` // Sales Rep
   }
 ];
