fb3a6245952f4a7425107b12a7abb2f182364ef3
elwood-berry <eberry@anterasoftware.com>
2021-07-22 13:45:44 -0500

    1 file modified

--- src/app/interfaces/lead/lead.ts
+++ src/app/interfaces/lead/lead.ts
@@ -1,11 +1,27 @@
 export interface Lead {
-  accountName: string;
-  email: string;
+  zip: string;
+  leadId: string;
   firstName: string;
   lastName: string;
-  leadId: number;
-  phone: string;
-  referredBy: string;
-  status: string;
   title: string;
+  department: string;
+  accountName: string;
+  salesRep: string;
+  email: string;
+  leadSource: string;
+  phone: string;
+  status: string;
+  coporateIdentity: string;
+  description: string;
+  referredBy: string;
+  billingAddress1: string;
+  billingAddress2: string;
+  billingCity: string;
+  billingState: string;
+  billingZip: string;
+  shippingAddress1: string;
+  shippingAddress2: string;
+  shippingCity: string;
+  shippingState: string;
+  shippingZip: string;
 }
